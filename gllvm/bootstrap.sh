742eba8a0ec76f055c4fecff5c6ec2a0de756482
Ian A.Mason <iam@csl.sri.com>
2020-08-16 12:53:53 -0700

    Base box for gllvm using llvm 10.

---
+++ gllvm/bootstrap.sh
@@ -0,0 +1,20 @@
+
+sudo apt-get update
+
+sudo apt-get install -y emacs25 dbus-x11
+
+sudo apt-get install -y git golang-go golang
+
+cmake make autoconf valgrind
+
+wget https://apt.llvm.org/llvm.sh
+
+sudo bash ./llvm.sh 10
+
+echo ". /vagrant/bash_profile" >> /home/vagrant/.bashrc
+
+git clone https://github.com/dominikh/go-mode.el.git /vagrant/go-mode.el
+
+cp /vagrant/emacs /home/vagrant/.emacs
+
+sudo chown -R vagrant:vagrant /usr/local /home/vagrant/.emacs /vagrant/go-mode.el
---
+++ gllvm/configure.sh
@@ -0,0 +1,5 @@
+
+go get github.com/SRI-CSL/gllvm/cmd/...
+
+git config --global user.email "iam@csl.sri.com"
+git config --global user.name "Ian A. Mason"
